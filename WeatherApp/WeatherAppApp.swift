f4ec4410ad4f625190bb7c2280fc69fdf1064e53
Bibek Paudyal <bibekpaudyal5@gmail.com>
2023-10-04 08:37:38 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by BIBEK PAUDYAL on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
